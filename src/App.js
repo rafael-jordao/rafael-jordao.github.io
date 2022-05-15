1fb4f326f199fad4e6dc98ff91fa78644dbaef05
Rafael Jordão <rafaeljordao.picado@gmail.com>
2022-05-15 16:09:29 -0300

    header with menu mobile

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React from "react";
 
-// https://ranekapi.origamid.dev/json/api/produto/tablet
-// https://ranekapi.origamid.dev/json/api/produto/smartphone
-// https://ranekapi.origamid.dev/json/api/produto/notebook
-
 const App = () => {
+  const [toggleButton, setToggleButton] = React.useState(false)
+
+  const handleClick = () => {
+    setToggleButton(!toggleButton)
+    console.log(toggleButton)
+  }
 
   return (
-    <div className="animate">
-      <div>hello world</div>
-      
+    <div className="App">
+      <header>
+        <div>
+          <h1 className="myName">Rafael Jordão</h1>
+        </div>
+
+        <nav className={`navMenu ${toggleButton ? 'navMenuActive' : ''}`}>
+          <button onClick={handleClick} className="btnMobile">Menu
+            <span id="hamburger"></span></button>
+
+          <ul className="menuTopics">
+            <a href="./"><li className="menuTopic">Home</li></a>
+            <a href="./"><li className="menuTopic">Sobre</li></a>
+            <a href="./"><li className="menuTopic">Contato</li></a>
+          </ul>
+        </nav>
+
+      </header>
+
     </div>
   )
+
+
 
 };
 
 
-export default App; 
+export default App;
