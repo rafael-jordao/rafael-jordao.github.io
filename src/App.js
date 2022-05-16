76459086b16c11f2c23903715f3bd9dd19948e7d
Rafael Jordão <rafaeljordao.picado@gmail.com>
2022-05-16 12:58:55 -0300

    creating form field

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import MainContent from "./components/MainContent/MainContent";
 import ServiceSectors from "./components/ServicesSector/ServicesSector";
 
 const App = () => {
-  const [ backToTop, setBackToTop ] = React.useState(false)
+  const [backToTop, setBackToTop] = React.useState(false)
 
   React.useEffect(() => {
-      window.addEventListener("scroll", () => {
-          if(window.scrollY > 100) {
-              setBackToTop(true)
-          }
-      })
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setBackToTop(true)
+      }
+    })
   }, [])
 
   const scrollUp = () => {
-      window.scrollTo({
-          top: 0,
-          behavior: "smooth",
-      })
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    })
   }
- 
+
   const styleBtn = {
     borderRadius: '50px',
     background: '#4a6ef1',
@@ -32,16 +32,40 @@ const App = () => {
     transition: '.3s',
     boxShadow: '0px 10px 20px var(--darkblue)'
   }
-  
+
   return (
     <div className="App">
       <Header />
       <MainContent />
       <ServiceSectors />
-      
+      <div className="dflexContainer">
+        <div className="textContainer">
+          <h1>Como eu posso ajudar?</h1>
+        </div>
+        <div >
+          <form className="formField" >
+            <div className="inputContainer">
+              <label className="labelColor">Insira seu nome</label>
+              <input type="text" className="mainInput"></input>
+            </div>
+            <div className="inputContainer">
+              <label className="labelColor">Insira seu e-mail</label>
+              <input type="e-mail" className="mainInput"></input>
+            </div>
+            <div className="inputContainer">
+              <label className="labelColor">Escreva sua mensagem</label>
+              <textarea className="mainInput"></textarea>
+            </div>
+            <div>
+              <button className="mainButton">Enviar</button>
+            </div>
+          </form>
+        </div>
+      </div>
+
       {backToTop && (
-                <button style={styleBtn} onClick={scrollUp}>^</button>
-            )}
+        <button style={styleBtn} onClick={scrollUp}>^</button>
+      )}
     </div>
   )
 };
