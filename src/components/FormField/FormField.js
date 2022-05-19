ee71abfdab366787a7d9677526330020e0c5a6d7
Rafael Jordão <rafaeljordao.picado@gmail.com>
2022-05-18 21:31:34 -0300

    form activate

--- src/components/FormField/FormField.js
+++ src/components/FormField/FormField.js
@@ -3,50 +3,55 @@ import "/Users/rafaeljordao/Documents/my-codes/rafael-jordao.github.io/src/compo
 
 
 const FormField = () => {
-    const [ inputName, setInputName ] = React.useState('')
-    const [ inputEmail, setInputEmail ] = React.useState('')
-    const [ inputText, setInputText ] = React.useState('')
-    
-    const handleChangeName = (e) => {
-        setInputName(e.target.value)
-    }
-    const handleChangeEmail = (e) => {
-        setInputEmail(e.target.value)
+    // const [inputName, setInputName] = React.useState('')
+    // const [inputEmail, setInputEmail] = React.useState('')
+    // const [inputText, setInputText] = React.useState('')
+    // const [message, setMessage] = React.useState('')
 
-    }
-    const handleChangeText = (e) => {
-        setInputText(e.target.value)
-    }
+    // const emoji = ':)'
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        setInputName(e.target.value)
-        setInputEmail(e.target.value)
-        setInputText(e.target.value)
-    }
-    
-    return(
+    // const handleChangeName = (e) => {
+    //     setInputName(e.target.value)
+    // }
+
+    // const handleChangeEmail = (e) => {
+    //     setInputEmail(e.target.value)
+    // }
+
+    // const handleChangeText = (e) => {
+    //     setInputText(e.target.value)
+    // }
+
+    // const handleSubmit = (e) => {
+    //     e.preventDefault()
+
+    //     setMessage(
+    //         <div className="paddingLabel"><label className="timeOutLabel">Obrigado {emoji}. Sua mensagem foi enviada.</label></div>
+    //     )
+
+    //     setTimeout(() => {
+    //         setMessage(null)
+    //     }, 2000)
+
+    //     setInputName(e.target.value)
+    //     setInputEmail(e.target.value)
+    //     setInputText(e.target.value)
+    //     setInputName('')
+    //     setInputEmail('')
+    //     setInputText('')
+    // }
+
+    return (
         <div className="dflexContainer">
             <div className="textContainer">
                 <h1>Como eu posso ajudar?</h1>
             </div>
             <div >
-                <form className="formField" >
-                    <div className="inputContainer">
-                        <label className="labelColor">Insira seu nome</label>
-                        <input value={inputName} onChange={handleChangeName}  type="text" className="mainInput"></input>
-                    </div>
-                    <div className="inputContainer">
-                        <label className="labelColor">Insira seu e-mail</label>
-                        <input value={inputEmail} onChange={handleChangeEmail}  type="e-mail" className="mainInput"></input>
-                    </div>
-                    <div className="inputContainer">
-                        <label className="labelColor">Escreva sua mensagem</label>
-                        <textarea  value={inputText} onChange={handleChangeText} className="mainInput"></textarea>
-                    </div>
-                    <div>
-                        <button onClick={handleSubmit} className="mainButton">Enviar</button>
-                    </div>
+                <form action="https://formsubmit.co/afa93664dc1045b20d9413a4c814aac7" method="POST" className="formField">
+                    <input placeholder="Insira seu nome" className="mainInput" type="text" name="name" required />
+                    <input placeholder="Insira seu e-mail" className="mainInput" type="email" name="email" required />
+                    <textarea placeholder="Escreva sua mensagem" className="mainInput" type="text" name="text" required />
+                    <button type="submit">Envie sua mensagem</button>
                 </form>
             </div>
         </div>
